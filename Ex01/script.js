84ff1e21de5aaaa568463cb7f822c132737adaf5
MateoDedi <mateod@live.com>
2023-10-03 15:19:09 +0200

    added some more

--- Ex01/script.js
+++ Ex01/script.js
@@ -11,8 +11,8 @@ bold.parentElement.nextElementSibling.style.color = "pink";
 
 bold.parentElement.parentElement.parentElement.previousElementSibling.children[0].children[2].style.color = "orange";
 
-bold.parentElement.parentElement.parentElement.previousElementSibling.children[0].children[2].querySelectorAll("a").forEach((a) => {
-    a.style.color = "green";
+bold.parentElement.parentElement.parentElement.previousElementSibling.children[0].children[2].querySelectorAll("a").forEach((element) => {
+    element.style.color = "green";
     });
 
 bold.parentElement.parentElement.parentElement.previousElementSibling.querySelector("h1").style.textTransform = "uppercase";
