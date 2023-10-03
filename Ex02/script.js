20d2c19da99104260cf1b536a3a1a33089c3384b
MateoDedi <mateod@live.com>
2023-10-03 16:02:32 +0200

    added more

--- Ex02/script.js
+++ Ex02/script.js
@@ -1,3 +1,17 @@
-for (let i = 0; i <= 100; i++) {
-    
+function generateButtons() {
+    const container = document.querySelector(".container");
+    const h1 = document.querySelector("h1");
+
+    // Update the h1 text
+    h1.textContent = "There are a lot of buttons here!";
+
+    // Create and append buttons to the container
+    for (let i = 1; i <= 100; i++) {
+        const button = document.createElement("button");
+        button.textContent = "I am button " + i;
+        container.appendChild(button);
+    }
 }
+
+// Use setTimeout to delay the execution of the function
+setTimeout(generateButtons, 2000);
