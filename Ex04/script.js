99c3989e45663958dd72138bf78841427cc6fc2f
MateoDedi <mateod@live.com>
2023-10-04 11:29:45 +0200

    added 04

---
+++ Ex04/script.js
@@ -0,0 +1,12 @@
+function generatePokemon() {
+    const container = document.querySelector(".container");
+
+    for (let i = 1; i <= 151; i++) {
+        const img = document.createElement("img");
+        img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;;
+        img.alt = `Pokemon ${i}`;
+        container.appendChild(img);
+    }
+}
+
+generatePokemon();
